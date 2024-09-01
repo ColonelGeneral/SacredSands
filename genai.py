2e896d61960d6cd2b8b2dccbacf52d7819253d26
ColonelGeneral <prayanshsukhija@gmail.com>
2024-09-01 07:54:22 +0530

    Signup working

--- genai.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDFcXAZvUHCbabalMgWP5y7aTnueVVaVmg")
-# Create GenerativeModel instance
-model = genai.GenerativeModel('gemini-1.5-flash')
-question = input("Enter your question: ")
-response = model.generate_content(question)
-print(response.text)
